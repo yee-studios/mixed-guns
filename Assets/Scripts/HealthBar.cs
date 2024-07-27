ca40b55cb9e6ced0bbbb6809914f2bb0e109f54b
Bruno <38665405+TheBrunoRM@users.noreply.github.com>
2024-07-27 02:38:05 -0300

    make healthbars not canvas

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,16 +6,24 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] public Entity target;
     [SerializeField] Vector3 offset;
-    [SerializeField] Transform fill;
+    [SerializeField] RectTransform fill;
     [SerializeField] float lerpSpeed = 10f;
+
+    Vector3 pos => target.transform.position + offset;
+
+    private void Start()
+    {
+        transform.position = pos;
+    }
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, pos, lerpSpeed * Time.deltaTime);
     }
 
     public void UpdateValue(float value)
     {
         fill.localScale = new Vector3(Mathf.Clamp01(value),1,1);
+        fill.anchoredPosition = new Vector3(Mathf.Clamp01(value*.5f),0,0);
     }
 }
