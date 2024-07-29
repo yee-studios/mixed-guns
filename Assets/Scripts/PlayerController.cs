72369495cf332b1c0a5e62a9decf41aba2672ebb
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2024-07-29 18:55:52 -0500

    Organize player controller fields and remove some hardcoded variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ using UnityEngine.Rendering.Universal;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : Singleton<PlayerController>
 {
-    #region cached components
+    #region Cached components
     Rigidbody2D rb;
     PlayerInput input;
     AudioSource audioSource;
@@ -18,46 +18,57 @@ public class PlayerController : Singleton<PlayerController>
     public Vector2 LastPosition { get; private set; }
     #endregion
 
-    [Header("Parameters")]
+    #region Unity fields
+    [Header("Movement")]
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float dashForce = 1000f;
     [SerializeField] int availableDashes = 0;
     [SerializeField] int maxDashes = 3;
+    public int MaxDashes => maxDashes;
+    [SerializeField] float dashReloadTime = 1f;
+    [SerializeField] float currentDashReload = 0f;
+    
+    [Header("Lightning")]
     [Range(0f,1f)]
-    [SerializeField] float constantSoundVolume = 0.25f;
     [SerializeField] float lightReductionRate = 0.25f;
     [SerializeField] float minLightOuter = 3f;
     [SerializeField] float maxLightOuter = 8f;
+    public Light2D flashLight;
+    public Light2D surroundingLight;
+    
+    [Header("Camera")]
     [Range(0f, 5f)]
     [SerializeField] float screenShakeIntensity = 1.5f;
+    [SerializeField] CinemachineVirtualCamera cinemachineVirtualCamera;
+    CinemachineBasicMultiChannelPerlin camNoise;
+    [SerializeField] float cameraMinSize = 4f;
+    [SerializeField] float cameraMaxSize = 5f;
+    
+    [Header("Powerups")]
     public int doubleSpeedTimeRemaining;
-    [SerializeField] bool doubleSpeed;
     public int fullVisionTimeRemaining;
-    bool fullVision;
-    bool fullVisionTweenInProgress;
 
     
     public bool startAnimation { private set; get; }
 
-    [Header("Audio Sources")]
+    [Header("Audio")]
+    [SerializeField] float constantSoundVolume = 0.25f;
     [SerializeField] AudioSource movingAudio;
     [SerializeField] AudioSource rotatingAudio;
 
     [SerializeField] ParticleSystem fart;
-    [SerializeField] CinemachineVirtualCamera cinemachineVirtualCamera;
-    CinemachineBasicMultiChannelPerlin cam_noise;
 
+    [Header("Other")]
     [SerializeField] GunController gun;
-    public Light2D flashLight;
-    public Light2D surroundingLight;
-
-    public int MaxDashes => maxDashes;
-    [SerializeField] float dashReloadTime = 1f;
-    [SerializeField] float currentDashReload = 0f;
 
     [SerializeField] float debugDeathTime = 0f;
+    #endregion
+    
+    bool doubleSpeed;
+    bool fullVision;
+    bool fullVisionTweenInProgress;
 
-    #region unity methods
+    #region Unity methods
     protected override void Awake()
     {
         base.Awake();
@@ -65,30 +76,30 @@ public class PlayerController : Singleton<PlayerController>
         audioSource = GetComponent<AudioSource>();
         entity = GetComponent<Entity>();
         entity.OnDied.AddListener(OnDied);
-        cam_noise = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        camNoise = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         StartCoroutine(EverySecond());
     }
 
-    float d1 = 3f;
-    float d2 = .25f;
+    readonly float delay1 = 3f;
+    readonly float delay2 = .25f;
 
     protected void Start()
     {
         input = FindObjectOfType<PlayerInput>();
         startAnimation = true;
         entity.invencibility = true;
-        DOTween.To(() => surroundingLight.falloffIntensity, x => surroundingLight.falloffIntensity = x, 0.5f, d1)
-            .ChangeStartValue(1f).SetDelay(d2).OnComplete(() => { entity.invencibility = false; startAnimation = false; });    
+        DOTween.To(() => surroundingLight.falloffIntensity, x => surroundingLight.falloffIntensity = x, 0.5f, delay1)
+            .ChangeStartValue(1f).SetDelay(delay2).OnComplete(() => { entity.invencibility = false; startAnimation = false; });    
 
-        DOTween.To(() => minLightOuter, x => minLightOuter = x, 3f, d1).ChangeStartValue(0f).SetDelay(d2);
+        DOTween.To(() => minLightOuter, x => minLightOuter = x, 3f, delay1).ChangeStartValue(0f).SetDelay(delay2);
         DOTween.To(() => surroundingLight.pointLightOuterRadius, x => surroundingLight.pointLightOuterRadius = x,
-            surroundingLight.pointLightOuterRadius, d1).ChangeStartValue(0f).SetDelay(d2).SetUpdate(UpdateType.Late);
+            surroundingLight.pointLightOuterRadius, delay1).ChangeStartValue(0f).SetDelay(delay2).SetUpdate(UpdateType.Late);
 
-        DOTween.To(() => flashLight.falloffIntensity, x => flashLight.falloffIntensity = x, 0.5f, d1).ChangeStartValue(1f).SetDelay(d2);
+        DOTween.To(() => flashLight.falloffIntensity, x => flashLight.falloffIntensity = x, 0.5f, delay1).ChangeStartValue(1f).SetDelay(delay2);
         DOTween.To(() => flashLight.pointLightInnerRadius, x => flashLight.pointLightInnerRadius = x,
-            flashLight.pointLightInnerRadius, d1).ChangeStartValue(0f).SetDelay(d2);
+            flashLight.pointLightInnerRadius, delay1).ChangeStartValue(0f).SetDelay(delay2);
         DOTween.To(() => flashLight.pointLightOuterRadius, x => flashLight.pointLightOuterRadius = x,
-            flashLight.pointLightOuterRadius, d1).ChangeStartValue(0f).SetDelay(d2);
+            flashLight.pointLightOuterRadius, delay1).ChangeStartValue(0f).SetDelay(delay2);
     }
 
     void OnDied()
@@ -205,10 +216,10 @@ public class PlayerController : Singleton<PlayerController>
     private void HandleCameraNoise()
     {
         float healthDiff = entity.Health / entity.MaxHealth;
-        cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(3f, 4f, healthDiff);
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(cameraMinSize, cameraMaxSize, healthDiff);
         float d = (1f-healthDiff) * screenShakeIntensity;
-        cam_noise.m_AmplitudeGain = d;
-        cam_noise.m_FrequencyGain = d;
+        camNoise.m_AmplitudeGain = d;
+        camNoise.m_FrequencyGain = d;
     }
 
     IEnumerator EverySecond() {
